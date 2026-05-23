59ae86f89e3e94a49c4358636e27b19f8ac26da4
Hwanki Shin <stormix@me.com>
2026-05-23 14:38:22 +0900

    feat: implement dynamic macOS Dock icon theme switching using Cocoa NSApplication API

--- src/app_icon.rs
+++ src/app_icon.rs
@@ -4,10 +4,52 @@ const APP_ICON_PNG: &[u8] = include_bytes!("../assets/app-icon.png");
 const APP_ICON_LIGHT_PNG: &[u8] = include_bytes!("../assets/app-icon-light.png");
 
 pub fn icon_for_dark_mode(is_dark: bool) -> IconData {
+    #[cfg(target_os = "macos")]
+    update_macos_dock_icon(is_dark);
+
     if is_dark {
         load_png_icon(APP_ICON_PNG)
     } else {
         load_png_icon(APP_ICON_LIGHT_PNG)
+    }
+}
+
+#[cfg(target_os = "macos")]
+pub fn update_macos_dock_icon(is_dark: bool) {
+    use objc2::runtime::AnyObject;
+
+    let png_bytes = if is_dark {
+        APP_ICON_PNG
+    } else {
+        APP_ICON_LIGHT_PNG
+    };
+
+    unsafe {
+        let app: *mut AnyObject = objc2::msg_send![objc2::class!(NSApplication), sharedApplication];
+        if app.is_null() {
+            return;
+        }
+
+        let ns_data: *mut AnyObject = objc2::msg_send![
+            objc2::class!(NSData),
+            dataWithBytes: png_bytes.as_ptr(),
+            length: png_bytes.len()
+        ];
+        if ns_data.is_null() {
+            return;
+        }
+
+        let ns_image: *mut AnyObject = objc2::msg_send![objc2::class!(NSImage), alloc];
+        if ns_image.is_null() {
+            return;
+        }
+        let ns_image: *mut AnyObject = objc2::msg_send![ns_image, initWithData: ns_data];
+        if ns_image.is_null() {
+            return;
+        }
+
+        let _: () = objc2::msg_send![app, setApplicationIconImage: ns_image];
+        let _: () = objc2::msg_send![ns_image, release];
     }
 }
 
